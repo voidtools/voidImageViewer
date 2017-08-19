373270fb563d61f803d04ce94df1e7fd08994db3
voidtools <31149593+voidtools@users.noreply.github.com>
2017-08-19 18:10:40 +0930

    Add files via upload

--- src/viv.c
+++ src/viv.c
@@ -17,6 +17,8 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 // TODO: (*=done)
+// OpenGL renderer
+// Direct3D renderer
 // graphics::GetHalftonePalette for 256 color mode.
 // high dpi icons
 // control toolbar customization
