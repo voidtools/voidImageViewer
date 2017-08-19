1ace2ee502ff5081f013b83ff428b497f41275c7
voidtools <31149593+voidtools@users.noreply.github.com>
2017-08-19 17:41:59 +0930

    Add files via upload

--- res/resource.h
+++ res/resource.h
@@ -1,1 +1,85 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by voidImageViewer.rc
+//
+#define IDD_FORMVIEW                    101
+#define IDD_FORMVIEW1                   101
+#define IDD_GENERAL                     101
+#define IDI_ICON1                       102
+#define IDD_DIALOG1                     106
+#define IDD_OPTIONS                     106
+#define IDD_FORMVIEW2                   107
+#define IDD_VIEW                        107
+#define IDD_CUSTOM_RATE                 109
+#define IDD_CONTROLS                    110
+#define IDB_BITMAP1                     112
+#define IDI_PREV                        116
+#define IDI_PLAY                        117
+#define IDI_NEXT                        118
+#define IDI_PAUSE                       119
+#define IDD_ABOUT                       121
+#define IDD_EDIT_KEY                    123
+#define IDI_ICON2                       124
+#define IDI_1TO1                        124
+#define IDI_BESTFIT                     126
+#define IDC_TAB1                        1003
+#define IDC_BMP                         1006
+#define IDC_GIF                         1007
+#define IDC_ICO                         1008
+#define IDC_JPEG                        1009
+#define IDC_JPG                         1010
+#define IDC_PNG                         1011
+#define IDC_TIF                         1012
+#define IDC_TIFF                        1013
+#define IDC_CHECKALL                    1014
+#define IDC_PAGEPLACEHOLDER             1015
+#define IDC_COMBO1                      1016
+#define IDC_COMBO3                      1018
+#define IDC_MULTIPLE_INSTANCES          1019
+#define IDC_TREE1                       1020
+#define IDC_STARTMENU                   1020
+#define IDC_CHECK1                      1021
+#define IDC_APPDATA                     1021
+#define IDC_COMBO2                      1022
+#define IDC_BUTTON1                     1023
+#define IDC_ADD_KEY                     1023
+#define IDC_BUTTON2                     1024
+#define IDC_CHECKNONE                   1024
+#define IDC_EDIT_KEY                    1024
+#define IDC_TAB2                        1025
+#define IDC_CUSTOM_RATE_EDIT            1026
+#define IDC_CUSTOM_RATE_TYPE_COMBO      1027
+#define IDC_LOOP_ANIMATIONS_ONCE        1028
+#define IDC_LEFTCLICKACTION             1029
+#define IDC_RIGHTCLICKACTION            1030
+#define IDC_LIST1                       1031
+#define IDC_COMMANDS_LIST               1031
+#define IDC_EDIT_KEY_CURRENTLY_USED_BY_LIST 1031
+#define IDC_LIST2                       1032
+#define IDC_KEYS_LIST                   1032
+#define IDC_BUTTON3                     1033
+#define IDC_REMOVE_KEY                  1033
+#define IDC_TAB3                        1034
+#define IDC_MOUSEWHEELACTION            1034
+#define IDC_ABOUTBACK                   1035
+#define IDC_ABOUTVOIDIMAGEVIEWER        1037
+#define IDC_ABOUTTITLE                  1038
+#define IDC_ABOUTVERSION                1039
+#define IDC_WINDOWEDBACKGROUNDCOLOR     1039
+#define IDC_ABOUTCOPYRIGHT              1040
+#define IDC_FULLSCREENBACKGROUNDCOLOR   1040
+#define IDC_ABOUTEMAIL                  1041
+#define IDC_EDIT_KEY_EDIT               1041
+#define IDC_ABOUTWEBSITE                1042
 
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        127
+#define _APS_NEXT_COMMAND_VALUE         40048
+#define _APS_NEXT_CONTROL_VALUE         1042
+#define _APS_NEXT_SYMED_VALUE           109
+#endif
+#endif
